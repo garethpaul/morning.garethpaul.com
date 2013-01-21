d3df5b2f578a0523ca39f02a40c3194b2016ed50
The Bears <thebears@bear-mini.local>
2013-01-20 20:14:00 -0800

    f

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 import os
-import tomtom
+from stuff.tomtom import traffic
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return tomtom.traffic('work')
+    return 'Delay ' + str(traffic('work'))
 
 @app.route('/home')
 def hello():
-    return tomtom.traffic('home')
+    return 'Delay ' + str(traffic('home')))
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
+    app.debug = True
     app.run(host='0.0.0.0', port=port)
 
