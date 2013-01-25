498a5a1f14e1815beacb9c4a27765bb93f44adcb
Gareth Jones <gareth@garethpaul.com>
2013-01-25 08:56:45 -0800

    adding bus car mpg stuff

--- app.py
+++ app.py
@@ -2,13 +2,15 @@ import os
 from stuff.tomtom import traffic
 from flask import render_template
 from flask import Flask
+import settings
 
 app = Flask(__name__)
 app.static_folder=os.getcwd() + '/static'
+cost_per_day = round(float(settings.work_miles*2/settings.miles_per_gallon*settings.cost_per_gallon),3)
 
 @app.route('/')
 def work():
-    return render_template('index.html', data=str(traffic('work')))
+	return render_template('index.html', data=str(traffic('work')), transport=cost_per_day, news=str(settings.news))
 
 @app.route('/home')
 def home():
