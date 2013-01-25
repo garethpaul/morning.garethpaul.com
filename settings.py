2f118f250b4fd73aedace8cc224426c140e33ccf
Gareth Jones <gareth@garethpaul.com>
2013-01-25 09:09:08 -0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 """
 LOCAL SETTINGS FILE FOR YOUR LOCATION
 """
-home_pos = '37.786194,-122.39897'
-work_pos = '37.33233,-122.03073'
-work_miles = 50
-miles_per_gallon = 30
-cost_per_gallon = 4.8
+home_pos = ''
+work_pos = ''
+work_miles = ''
+miles_per_gallon = ''
+cost_per_gallon = ''
